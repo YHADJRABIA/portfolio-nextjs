5e5c4523c200347872733f7f1c1bd3acb4506049
YHR <h_yacine@live.fr>
2022-05-22 21:48:40 +0200

    Update cypress + fix format

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,18 +1,18 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 
 type GTagEvent = {
-  action: string;
-  category: string;
-  label: string;
-  value: number;
-};
+  action: string
+  category: string
+  label: string
+  value: number
+}
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
-  });
-};
+  })
+}
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent): void => {
@@ -20,5 +20,5 @@ export const event = ({ action, category, label, value }: GTagEvent): void => {
     event_category: category,
     event_label: label,
     value,
-  });
-};
+  })
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-require("dotenv").config({ path: __dirname + "/config/.env.local" });
-const path = require("path");
-const nextTranslate = require("next-translate");
+require("dotenv").config({ path: __dirname + "/config/.env.local" })
+const path = require("path")
+const nextTranslate = require("next-translate")
 
 module.exports = {
   reactStrictMode: true,
@@ -9,8 +9,8 @@ module.exports = {
     includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ["res.cloudinary.com", "flagcdn.com"], // Enables use of Cloudinary images
+    domains: ["res.cloudinary.com", "flagcdn.com"], // Enables use of external images
   },
 
   ...nextTranslate(), // Refers to i18n.json
-};
+}
