3d9ed68d25f187f8270dbcb9675e28932c4412b5
YHR <h_yacine@live.fr>
2022-05-28 20:48:33 +0200

    Refactor Contexts into single component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,15 @@
 import React, { FC } from "react"
 import Footer from "@/components/Footer/Footer"
 
-// Global state
-import { Provider } from "react-redux"
-import store from "@/redux/store"
-import { AuthProvider } from "@/context/UserContext"
+// Global state management
+import ContextTree from "../ContextTree"
 
 const Layout: FC = ({ children }) => {
   return (
-    <Provider store={store}>
-      <AuthProvider>
-        <main>{children}</main>
-        <Footer color="#fff" />
-      </AuthProvider>
-    </Provider>
+    <ContextTree>
+      <main>{children}</main>
+      <Footer color="white" />
+    </ContextTree>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,19 +7,12 @@ import React, { useEffect } from "react"
 import type { AppProps } from "next/app"
 import { useRouter } from "next/router"
 
-// Global state
-import { Provider } from "react-redux"
-import store from "@/redux/store"
-/* import { AuthProvider } from "@/context/UserContext"; */
-import { ThemeProvider } from "@/context/ThemeContext"
-
 import * as gtag from "@/lib/gtag" // Google Analytics
 
-// Components
-import { ToastContainer } from "react-toastify"
 import Layout from "@/components/Layout/Layout"
 import Footer from "@/components/Footer/Footer"
 import { isProduction } from "@/utilities/general"
+import ContextTree from "@/components/ContextTree"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -39,32 +32,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   switch (Component.name) {
     case "HomePage":
       return (
-        <Provider store={store}>
-          <ThemeProvider>
-            <ToastContainer />
-            <Component {...pageProps} />
-            <Footer color="fff" />
-          </ThemeProvider>
-        </Provider>
+        <ContextTree>
+          <Component {...pageProps} />
+          <Footer color="white" />
+        </ContextTree>
       )
     case "PageNotFound":
-      return (
-        <>
-          <Component {...pageProps} />
-          <Footer color="#f2f2f5" />
-        </>
-      )
+      return <Component {...pageProps} />
 
     default:
       return (
-        <Provider store={store}>
-          <ThemeProvider>
-            <Layout>
-              <ToastContainer />
-              <Component {...pageProps} />
-            </Layout>
-          </ThemeProvider>
-        </Provider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       )
   }
 }
