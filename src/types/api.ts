7d91ad1b0363240a539965d3d093499a7b12511a
YHR <h_yacine@live.fr>
2022-05-31 03:17:37 +0200

    Add API model + Query data

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,9 +1,3 @@
 export type APIStatus = "success" | "error"
 
 export type EmailAPIResponse = { status: APIStatus; msg: string }
-
-export type Skill = {
-  name: string;
-  icon: string;
-  category: string;
-}
