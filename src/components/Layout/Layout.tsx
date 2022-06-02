237dee67f882ae83bf79a2ba455e1c5ffb4a982f
YHR <h_yacine@live.fr>
2022-06-02 21:00:23 +0200

    Relocate file and remove unused imports

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react"
 import Footer from "@/components/Footer/Footer"
 
 // Global state management
-import ContextTree from "../ContextTree"
+import ContextTree from "@/context/ContextTree"
 
 const Layout: FC = ({ children }) => {
   return (
