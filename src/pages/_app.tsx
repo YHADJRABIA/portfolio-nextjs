925147db55fc73785d37beeb65df3818eb85e481
YHR <h_yacine@live.fr>
2022-06-07 01:36:11 +0200

    Relocate `ContextTree` & remove useless imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,9 +10,7 @@ import { useRouter } from "next/router"
 import * as gtag from "@/lib/gtag" // Google Analytics
 
 import Layout from "@/components/Layout/Layout"
-import Footer from "@/components/Footer/Footer"
 import { isProduction } from "@/utilities/general"
-import ContextTree from "@/components/ContextTree"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
