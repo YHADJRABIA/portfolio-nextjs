0b643fe4a9f9001d3330df552f3ebb438d95f060
YHR <h_yacine@live.fr>
2022-06-28 00:21:25 +0200

    Add `useClickAnywhere` hook

---
+++ src/hooks/useClickAnywhere.ts
@@ -0,0 +1,10 @@
+import { useEventListener } from "./useEventListener"
+
+type Handler = (event: MouseEvent) => void
+
+// Calls handler function whenever user clicks anywhere on the page
+export const useClickAnyWhere = (handler: Handler) => {
+  useEventListener<MouseEvent>("click", event => {
+    handler(event)
+  })
+}
