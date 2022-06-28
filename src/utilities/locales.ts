4b88229f7959951802b2a4d5260361273fcf5454
YHR <h_yacine@live.fr>
2022-06-28 16:05:47 +0200

    Create utility function to generate localised SSG paths

--- src/utilities/locales.ts
+++ src/utilities/locales.ts
@@ -1,1 +1,18 @@
+import { Locale } from "@/types/locales"
+
 export const locales = ["en", "fr", "ru", "sv"]
+
+// Used to generate dynamic paths in SSG for all locales in all slugs
+export const generateLocalisedPaths = (
+  data: { slug: string }[],
+  locales: Locale[]
+) => {
+  data
+    .map((project: { slug: string }) => {
+      return locales.map(locale => ({
+        params: { slug: project.slug },
+        locale,
+      }))
+    })
+    .flat() // Flatten array to avoid nested arrays
+}
