ecd6f0f25c495f848b6e5773b74b9e069cb1b741
YHR <h_yacine@live.fr>
2022-06-29 00:03:50 +0200

    Update `middleware.ts`

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,18 @@
-import { NextRequest, NextResponse } from "next/server"
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import { NextRequest, NextResponse, userAgent } from "next/server"
 
 // Triggers up following every request
 export default async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-
   const time = Date.now()
   const timeStr = new Date(time).toLocaleDateString()
+  const { device } = userAgent(req)
 
   const logData = await {
     time: timeStr,
     url: req.url,
     ip: req.ip,
-    ua: req.ua,
+    ua: device,
     country: req.geo?.country,
     city: req.geo?.city,
   }
