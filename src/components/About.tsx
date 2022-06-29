87e8fbbddcdde79e1991e8fedeb39cfd6ecc8ee1
YHR <h_yacine@live.fr>
2022-06-29 11:05:56 +0200

    Update instance of `HoverImage` to accomodate for change

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,12 +2,13 @@ import useTranslation from "next-translate/useTranslation"
 import Trans from "next-translate/Trans"
 import InvisibleAnchor from "./UI/InvisibleAnchor"
 import HoverImage from "./UI/HoverImage"
-
 import styles from "./About.module.scss"
 import SectionHeader from "./UI/SectionHeader"
 import { useContext } from "react"
 import { ThemeContext } from "@/context/ThemeContext"
 import cn from "classnames"
+import mePic from "~/me.jpg"
+import meNPysen from "~/me-with-pysen.jpg"
 
 const About = () => {
   const { t } = useTranslation("common")
@@ -21,8 +22,8 @@ const About = () => {
       <InvisibleAnchor id="about" />
       <div className={styles.photoContainer}>
         <HoverImage
-          src="/me-with-pysen.jpg"
-          hoverSrc="/me.jpg"
+          src={meNPysen}
+          hoverSrc={mePic}
           alt={t("myself")}
           title={t("myself")}
           data-testid="about-photo"
