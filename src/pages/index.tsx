fe4dabf26ef897ad2d4306c813956f58d5c931c2
YHR <h_yacine@live.fr>
2022-07-05 03:58:55 +0200

    Log other value

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,8 @@ const HomePage: NextPage<PropTypes> = ({ data }: PropTypes) => {
   const projects = data.allProjects
 
   // TODO remove console.log
-  console.log(projects)
+  console.log("data", data)
+  console.log("projects", projects)
 
   return (
     <>
