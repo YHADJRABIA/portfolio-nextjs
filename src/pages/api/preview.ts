e3d69ac8a9d116217654346a62e941c24312c010
YHR <h_yacine@live.fr>
2022-07-13 22:47:33 +0200

    Better describe the preview mode

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-// Endpoint to allow preview of a draft from the used headless CMS. The preview is rendered at request time instead of build time
+// Endpoint to enable preview mode. Useful to preview a headless CMS draft. The preview is rendered at request time instead of build time
+// The preview mode will remain enabled unless another endpoint to disable it is called
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setPreviewData({}) // Sets browser's cookie to allow preview mode. Any request containing this cookie will be rendered in preview mode
   res.writeHead(307, { Location: "/" })
