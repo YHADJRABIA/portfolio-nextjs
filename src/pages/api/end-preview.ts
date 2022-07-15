c78627273af06b2833fba226c83cb5232d1d0a41
YHR <h_yacine@live.fr>
2022-07-15 11:56:17 +0200

    Remove useless `req` parameter

--- src/pages/api/end-preview.ts
+++ src/pages/api/end-preview.ts
@@ -1,6 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiResponse } from "next"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(res: NextApiResponse) {
   res.clearPreviewData() // Clears browser's cookies to disable preview mode.
   res.end("Preview mode disabled")
 }
