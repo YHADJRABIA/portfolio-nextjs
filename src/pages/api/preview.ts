7f77e69c04a0c5d033aba399f380f4ad4f5dfc4a
YHR <h_yacine@live.fr>
2022-07-15 12:35:52 +0200

    Enforce strict value + better handle errors

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -2,19 +2,21 @@ import { APIResponse } from "@/types/api"
 import { NextApiRequest, NextApiResponse } from "next"
 
 // Endpoint to enable/disable preview mode. Useful to preview a headless CMS draft. The preview is rendered at request time instead of build time
-// The preview mode will remain enabled unless the endpoint is called with a `?disable=(anything-here)` query parameter
+// The preview mode will remain enabled unless the endpoint is called with a `?disable=true` query parameter
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<APIResponse>
 ) {
+  const { disable, secret } = req.query
+
   // Disable mode
-  if (req.query.disable) {
+  if (disable === "true" && !secret) {
     res.clearPreviewData() // Clears browser's cookies to disable preview mode.
     res.end("Preview mode disabled")
   }
 
   // Enable mode
-  if (req.query.secret !== process.env.NEXT_DATOCMS_DRAFT_TOKEN) {
+  if (secret !== process.env.NEXT_DATOCMS_DRAFT_TOKEN) {
     // Token is used to restrict access to CMS's draft. This should only be known to this API route and the CMS
     return res.status(401).json({ status: "error", msg: "Invalid token" })
   }
