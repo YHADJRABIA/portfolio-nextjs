326b162032386d592e84f1be79bfbd8b46c022f6
YHR <h_yacine@live.fr>
2022-07-15 12:20:19 +0200

    Improve clarity

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -7,10 +7,13 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<APIResponse>
 ) {
+  // Disable mode
   if (req.query.disable) {
     res.clearPreviewData() // Clears browser's cookies to disable preview mode.
     res.end("Preview mode disabled")
   }
+
+  // Enable mode
   if (req.query.secret !== process.env.NEXT_DATOCMS_DRAFT_TOKEN) {
     // Token is used to restrict access to CMS's draft. This should only be known to this API route and the CMS
     return res.status(401).json({ status: "error", msg: "Invalid token" })
