a57f95451786426297286398cffe24683f5bfdf9
YHR <h_yacine@live.fr>
2022-07-15 12:03:32 +0200

    Remove now redundant `end-preview` endpoint

--- src/pages/api/end-preview.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiResponse } from "next"
-
-export default function handler(res: NextApiResponse) {
-  res.clearPreviewData() // Clears browser's cookies to disable preview mode.
-  res.end("Preview mode disabled")
-}
