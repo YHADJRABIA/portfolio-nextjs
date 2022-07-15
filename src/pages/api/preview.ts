0894304db294aa526f431d9f200c1ea5cebee215
YHR <h_yacine@live.fr>
2022-07-15 11:52:40 +0200

    Improve endpoint's security

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -7,11 +7,11 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<APIResponse>
 ) {
-  res.setPreviewData({}) // Sets browser's cookie to enable preview mode. Any request containing this cookie will be rendered in preview mode
   if (req.query.secret !== process.env.NEXT_DATOCMS_DRAFT_TOKEN) {
     // Token is used to restrict access to CMS's draft. This should only be known to this API route and the CMS
     return res.status(401).json({ status: "error", msg: "Invalid token" })
   }
+  res.setPreviewData({}) // Sets browser's cookie to enable preview mode. Any request containing this cookie will be rendered in preview mode
 
   res.end("Preview mode enabled")
 }
