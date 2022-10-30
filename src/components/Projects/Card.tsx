aac9a83e0fc01b997a18e0c0c65246adacd08a1d
YHR <h_yacine@live.fr>
2022-10-30 13:29:33 +0100

    Update `Link` to fix breaking changes

--- src/components/Projects/Card.tsx
+++ src/components/Projects/Card.tsx
@@ -24,36 +24,36 @@ const Card = ({ name, img, url, repo, slug }: PropTypes) => {
     <>
       {contentExists && (
         <li className={cn(styles.card, { [styles.darkTheme]: darkTheme })}>
-          <Link href={`/projects/${slug}`} passHref>
-            <a className={styles.imageContainer} title={name}>
-              <Image data={img.responsiveImage} />
-            </a>
+          <Link
+            href={`/projects/${slug}`}
+            className={styles.imageContainer}
+            title={name}
+          >
+            <Image data={img.responsiveImage} />
           </Link>
 
           <h3 className={styles.projectName}>{name}</h3>
 
-          <Link href={`/projects/${slug}`} passHref>
-            <a className={styles.projectDetails} title={name}>
-              {t("moreDetails")}
-            </a>
+          <Link
+            href={`/projects/${slug}`}
+            className={styles.projectDetails}
+            title={name}
+          >
+            {t("moreDetails")}
           </Link>
 
           <div className={styles.cardFooter}>
             {url && (
               <div className={styles.projectLink}>
-                <Link href={url} passHref>
-                  <a title={url}>
-                    <i className="fas fa-link"></i>
-                  </a>
+                <Link href={url} title={url}>
+                  <i className="fas fa-link"></i>
                 </Link>
               </div>
             )}
             {repo && (
               <div className={styles.projectRepo}>
-                <Link href={repo} passHref>
-                  <a title={repo}>
-                    <i className="fab fa-github"></i>
-                  </a>
+                <Link href={repo} title={repo}>
+                  <i className="fab fa-github"></i>
                 </Link>
               </div>
             )}
