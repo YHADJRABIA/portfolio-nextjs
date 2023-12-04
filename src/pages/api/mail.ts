058a2cde0945cb4d3db102fd6dde7bd6e745814b
YHR <h_yacine@live.fr>
2023-12-04 01:09:22 +0100

    Implement Nodemailer

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 
 import { isEmpty, isEmail, validateHuman } from "@/utilities/formValidator" // Validators
-import { sendEmail, acknowledgeReceipt } from "@/utilities/sendGrid"
 import { APIResponse } from "@/types/api"
 import { isProduction } from "@/utilities/general"
+import { acknowledgeReceipt, sendEmail } from "@/utilities/mailer"
 
 export default async function handler(
   req: NextApiRequest,
@@ -36,11 +36,11 @@ export default async function handler(
 
   try {
     // Sending e-mail & acknowledging receipt (only in production)
-    if (isProduction)
-      await Promise.all([
-        sendEmail(name, email, message),
-        acknowledgeReceipt(name, email, lang),
-      ])
+    /*     if (isProduction) */
+    await Promise.all([
+      sendEmail(name, email, message),
+      acknowledgeReceipt(name, email, lang),
+    ])
 
     return res.status(200).json({ status: "success", msg: "E-mail sent!" })
   } catch (err) {
