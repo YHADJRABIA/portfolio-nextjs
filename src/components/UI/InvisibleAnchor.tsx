6ddcc8185bbb0c5c0636baf102c6560eb708cae7
YHR <h_yacine@live.fr>
2024-03-25 15:00:37 +0100

    Put link in constant

--- src/components/UI/InvisibleAnchor.tsx
+++ src/components/UI/InvisibleAnchor.tsx
@@ -2,11 +2,11 @@ interface PropTypes {
   id: string
 }
 
+const DUMMY_HREF = "https://github.com/YHADJRABIA/" // To enforce href on a tag
+
 // Invisible anchor to offset nav height
 const InvisibleAnchor = ({ id }: PropTypes) => {
-  return (
-    <a id={id} href="https://github.com/YHADJRABIA/" className="anchor"></a>
-  )
+  return <a id={id} href={DUMMY_HREF} className="anchor"></a>
 }
 
 export default InvisibleAnchor
