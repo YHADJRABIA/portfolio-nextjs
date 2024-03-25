3d182dc6d77887c95f2d12c6fb6260ccd1aa5bfc
YHR <h_yacine@live.fr>
2024-03-25 15:23:33 +0100

    Refactor

--- src/utilities/general.ts
+++ src/utilities/general.ts
@@ -1,6 +1,7 @@
+const PRODUCTION_URL = "https://yhadjrabia.vercel.app/"
+const DEVELOPMENT_URL = "http://localhost:3000"
+
 export const isProduction = process.env.NODE_ENV === "production"
 export const isDevelopment = process.env.NODE_ENV === "development"
 export const isClient = typeof window !== "undefined"
-export const websiteUrl = isProduction
-  ? "https://yhadjrabia.vercel.app/"
-  : "http://localhost:3000"
+export const websiteUrl = isProduction ? PRODUCTION_URL : DEVELOPMENT_URL
